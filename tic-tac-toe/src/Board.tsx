007427a5075ae46e953cb78fb5cf8e6ed83488b1
Utkarsh <20616980+utkarsh1097@users.noreply.github.com>
2024-01-26 02:58:27 +0900

    Add initial board with hardcoded states

---
+++ tic-tac-toe/src/Board.tsx
@@ -0,0 +1,18 @@
+import Square from "./Square";
+
+function Board() {
+    const boardSquares : Array<JSX.Element> = [];
+
+    for(let i = 0; i < 3; i++) {
+        const rowSquares : Array<JSX.Element> = [];
+        for(let j = 0; j < 3; j++) {
+            rowSquares.push(<Square key={`square-${3*i+j}}`}/>);
+        }
+        boardSquares.push(<div key={`row-${i}`}>{rowSquares}</div>);
+    }
+
+    // JSX.Element[] -> JSX.Element. Empty tags ok for the conversion
+    return <>{boardSquares}</>;
+}
+
+export default Board;
---
+++ tic-tac-toe/src/Square.tsx
@@ -0,0 +1,5 @@
+function Square() {
+    return <button className="square">X</button>
+}
+
+export default Square;
