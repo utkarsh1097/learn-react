19e4eeff15b7c1d4ef496e1016a20f59a0731d0d
Utkarsh <20616980+utkarsh1097@users.noreply.github.com>
2024-01-28 02:03:22 +0900

    Refactor logging

--- tic-tac-toe/src/Board.tsx
+++ tic-tac-toe/src/Board.tsx
@@ -9,6 +9,10 @@ function Board() {
     // We maintain the states of each square at Board level in order to be able to keep track of the winning state
     const [boardState, setBoardState] = React.useState(Array<InputType>(9).fill(InputType.Empty));
 
+    function checkWinner() {
+        console.log("Checking winner!");
+    }
+
     function onPlay(index: number) {
         const nextBoardState = boardState.slice();
         nextBoardState[index] = nextInput;
@@ -25,12 +29,10 @@ function Board() {
         const rowSquares : Array<JSX.Element> = [];
         for(let j = 0; j < 3; j++) {
             const squareNum = 3*i+j;
-            rowSquares.push(<Square squareNum={squareNum} inputValue={boardState[squareNum]} clickAction={() => {onPlay(squareNum)}} key={`square-${3*i+j}}`}/>);
+            rowSquares.push(<Square squareNum={squareNum} inputValue={boardState[squareNum]} clickAction={() => { console.log(`Move played on square ${squareNum}!`); onPlay(squareNum)}} key={`square-${3*i+j}}`}/>);
         }
         boardSquares.push(<div key={`row-${i}`}>{rowSquares}</div>);
     }
-
-    console.log("Move played on board!");
 
     // JSX.Element[] -> JSX.Element. Empty tags ok for the conversion
     return (
--- tic-tac-toe/src/Square.tsx
+++ tic-tac-toe/src/Square.tsx
@@ -2,8 +2,6 @@ import React, { MouseEventHandler } from "react";
 import InputType from "./InputType";
 
 function Square({ squareNum,  inputValue, clickAction } : { squareNum: number, inputValue: InputType, clickAction: MouseEventHandler }) {
-
-    console.log(`Move played on square ${squareNum}!`);
     return <button className="square" onClick={clickAction}>{inputValue}</button>
 }
 
