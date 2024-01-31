d53e29814a431ccbf2a874504f6e460578b3f83c
Utkarsh <20616980+utkarsh1097@users.noreply.github.com>
2024-02-01 05:58:15 +0900

    "Landing page" test code

--- HelloRN/App.tsx
+++ HelloRN/App.tsx
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Landing from './components/Landing';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={{"color": "white", backgroundColor: "green"}}>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Landing />
+    // <View style={styles.container}>
+    //   <Text style={{"color": "white", backgroundColor: "green"}}>Open up App.tsx to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
